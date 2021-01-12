a831ca9a0a78097afa261f568b4aa02a33c2d047
poutingemoji <9awu680@gmail.com>
2021-01-12 05:42:27 -0800

    heroku

--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
 import Client from "./Base/Client"
-console.log(process.env.TESTPREFIX)
+console.log(process.env.PREFIX)
 console.log(process.env.MONGODB_URI);
 new Client({
-  commandPrefix: process.env.PREFIX,
+  commandPrefix: process.env.COMMAND_PREFIX,
   owner: "257641125135908866",
   shards: "auto",
 }).start();
