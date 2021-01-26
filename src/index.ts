f0a6205b81347e1adf0d85aa71b91f3953218e09
poutingemoji <9awu680@gmail.com>
2021-01-26 01:21:10 -0800

    prefix

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import Client from "./base/Client";
-console.log(process.env.COMMAND_PREFIX);
+console.log(process.env.PREFIX);
 new Client({
   commandPrefix: process.env.COMMAND_PREFIX,
   owner: "257641125135908866",
